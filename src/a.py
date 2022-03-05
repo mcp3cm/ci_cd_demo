5af6bb85d2c78c9605c0d52a283ad0c6b4c4bed9
harun <harun@haruns-Air.attlocal.net>
2022-03-04 20:44:10 -0500

    demo github action

---
+++ src/a.py
@@ -0,0 +1,10 @@
+class Assignment:
+    def incr(self, n):
+        return n+1
+
+    def square(self, n):
+        return n*n
+
+    def incorrect_code(self):
+        k = []
+        return k['k']
--- test_a.py
+++ test_a.py
@@ -1,15 +1,51 @@
-from a import *
+import importlib
+import fpdf
+import math
 
-def test_fibonacci():
-    assert fibonacci(3) == 2
-    assert fibonacci(4) == 3
-    assert fibonacci(3) == 1
-    assert fibonacci(3) == 2
+# Load the module
 
+src = importlib.import_module('src.a')
+tst = importlib.import_module('tests')
 
-def test_square():
-    assert square(3) == 9
-    assert square(3) == 9
+ass = src.Assignment()
+test_functions = dir(tst)
 
-def test_incr():
-    assert incr(1) == 2
+tests_found = []
+successful_tests = []
+failed_tests = []
+
+for tf in test_functions:
+    if not tf.startswith('__') and not tf.endswith('__'):
+        if callable(func := getattr(tst, tf)):
+            tests_found.append(tf)
+            try:
+                func(ass)
+                successful_tests.append(tf)
+            except:
+                failed_tests.append(tf)
+
+def report():
+    pdf = fpdf.FPDF()
+    pdf.add_page()
+    pdf.set_font('Arial', '', 11)  
+    pdf.cell(200, 10, txt = "Report for your assignment", ln = 1, align = 'C')
+
+    pdf.set_text_color( 46, 204, 113 )
+    for ts in successful_tests:
+        pdf.cell(200, 10, txt = f'+ {ts}', ln = 1)
+
+    pdf.set_text_color(  231, 76, 60 )
+    for ts in failed_tests:
+        pdf.cell(200, 10, txt = f'x {ts}', ln = 1)
+  
+    pdf.set_text_color( 0,0,0 )
+    txt = f'Your total performance was {len(successful_tests)}/{len(failed_tests)}'
+    pdf.cell(200, 10, txt = txt, ln = 1)
+
+    ratio = len(successful_tests)/len(failed_tests)
+    txt = f'Your score is {math.ceil(ratio*100)}'
+    pdf.cell(200, 10, txt = txt, ln = 1)
+
+    pdf.output("a.pdf")
+
+report()
