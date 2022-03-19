7da1f196e2bdf82c73649a043bb50192be004c7c
Harun Tuncay <haruntuncay@live.com>
2022-03-19 15:12:39 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,10 +1,22 @@
 class Assignment:
+    
+    '''
+        Should increment and return the value of the parameter n.
+        Example: incr(3) -> 4
+    '''
     def incr(self, n):
-        return n+1
+        pass
 
+    '''
+        Should return the squared value of the parameter n.
+        Example: square(3) -> 9
+    '''
     def square(self, n):
-        return n*n
+        pass
 
+    '''
+        Semantically incorrect code
+    '''
     def incorrect_code(self):
         k = []
         return k['k']
