87b375dbe6f49e2fae02d0b627697bede20ac51a
mcp3cm <100780449+mcp3cm@users.noreply.github.com>
2022-03-21 09:07:08 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,9 +5,12 @@ class Assignment:
         Example: incr(3) -> 4 
     '''
     def incr(self, n):
-   
+        return n + 1
 
-    
+    '''
+        Should return the squared value of the parameter n.
+        Example: square(3) -> 9
+    '''
     def square(self, n):
         return n**2
 
